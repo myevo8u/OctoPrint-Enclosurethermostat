2cce4e102c60cb8db5d8c7d3241fb49c55524b59
myevo8u <49907883+myevo8u@users.noreply.github.com>
2022-07-21 22:48:20 -0400

    Add files via upload

---
+++ octoprint_EnclosureThermostat/__init__.py
@@ -0,0 +1,306 @@
+import octoprint.plugin
+from octoprint.util import RepeatedTimer
+import sys
+import os
+import serial as s
+import time
+from flask import jsonify, request, make_response, Response
+
+
+#Call all Plugins
+class EnclosurethermostatPlugin(octoprint.plugin.StartupPlugin,
+                                octoprint.plugin.TemplatePlugin,
+                                octoprint.plugin.AssetPlugin,
+                                octoprint.plugin.SettingsPlugin,
+                                octoprint.plugin.BlueprintPlugin):
+                                
+    def __init__(self):
+        self._checkTempTimer = None
+        self.temp = 0
+        self.mode = ""
+        self.status = ""
+        self.TargetTemp = ""
+        self.FanSpeed = ""
+        self.serialconnected = False
+        self.arduino = s.Serial()  #Define Serial
+        self.RequestCommandProcess = False
+    
+    def start_tempcheck_timer(self, interval):
+        self._checkTempTimer = RepeatedTimer(interval, self.get_enclosure_temp, run_first=True)
+        self._checkTempTimer.start() 
+
+    def connect_serial_thermo(self):
+        #Serial Connection
+        try:
+            self.arduino.port=self.comport
+            self.arduino.baudrate=self.baudrate
+            self.arduino.timeout=0
+            #arduino.setDTR(False)
+            #arduino.rtscts = True
+            self.arduino.open();
+            self._logger.info("Enclosure Thermostat Serial Connected.")
+            os.system('stty -F /dev/Temp_Controller -hupcl')
+            time.sleep(1)
+            self.start_tempcheck_timer(5)
+            self.serialconnected = True
+        except Exception as e:
+            self.serialconnected = False
+            self._logger.error("Enclosure Thermostat Connection Failed: %" % (e))
+            self.stop_tempcheck_timer()
+    
+    ##~~ Blueprint Reader
+
+    @octoprint.plugin.BlueprintPlugin.route("/thermostatoff", methods=["GET"])
+    def mythermostatoff(self):
+        if (self.RequestCommandProcess == False):
+            self.RequestCommandProcess = True  
+            try:
+                if self.serialconnected:
+                    self._logger.info("Getting Enclosure Temp..")
+                    command = "<M0>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    self.arduino.flush()
+                    self._logger.info(self.temp)
+                    self.RequestCommandProcess = False
+                    return jsonify(success=True)
+                self.RequestCommandProcess = False
+                
+            except:
+                self._logger.error("Enclosure Thermostat Encountered an Issue: 1")
+                self.RequestCommandProcess = False
+                return jsonify(success=False)
+
+        
+    @octoprint.plugin.BlueprintPlugin.route("/thermostatfilament", methods=["GET"])
+    def mythermostatfilament(self):
+        if (self.RequestCommandProcess == False):
+            self.RequestCommandProcess = True  
+            try:
+                if self.serialconnected:
+                    data = request.values["mode"]
+                    self._logger.info("Setting Mode..")
+                    command = "<M1>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    if response == "200":
+                        self._logger.info("Mode changed: " + command)
+                        self.arduino.flush()
+                        command = "<F" + data +">"
+                        self.arduino.write(command.encode('utf-8'))
+                        time.sleep(0.1)
+                        response = self.arduino.readline().decode().strip()
+                        if response == "200":
+                            self._logger.info("Filament changed: " + command)
+                            self.RequestCommandProcess = False
+                            return jsonify(success=True)
+                self.RequestCommandProcess = False
+            except:
+                self._logger.error("Enclosure Thermostat Encountered an Issue: 2")
+                self.RequestCommandProcess = False
+                return jsonify(success=False)
+        
+    @octoprint.plugin.BlueprintPlugin.route("/thermostatmantemp", methods=["GET"])
+    def mythermostatmantemp(self):
+        if (self.RequestCommandProcess == False):
+            self.RequestCommandProcess = True  
+            try:
+                if self.serialconnected:
+                    data = request.values["tempval"]
+                    self._logger.info("Setting Temp..")
+                    command = "<M2>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    if response == "200":
+                        self._logger.info("Mode changed: " + command)
+                        self.arduino.flush()
+                        command = "<T" + data + ">"
+                        self.arduino.write(command.encode('utf-8'))
+                        time.sleep(0.1)
+                        response = self.arduino.readline().decode().strip()
+                        if response == "200":
+                            self._logger.info("Target Temp changed: " + command)
+                            self.RequestCommandProcess = False
+                            return jsonify(success=True)                          
+                self.RequestCommandProcess = False       
+            except:
+                self._logger.error("Enclosure Thermostat Encountered an Issue: 2")
+                self.RequestCommandProcess = False
+                return jsonify(success=False)
+
+        
+    @octoprint.plugin.BlueprintPlugin.route("/thermostatmanpwm", methods=["GET"])
+    def mythermostatmanpwm(self):
+        if (self.RequestCommandProcess == False):
+            self.RequestCommandProcess = True   
+            try:
+                if self.serialconnected:
+                    data = request.values["tempval"]
+                    self._logger.info("Setting Fan Speed..")
+                    command = "<M3>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    if response == "200":
+                        self._logger.info("Mode changed: " + command)
+                        self.arduino.flush()
+                        command = "<P" + data + ">"
+                        self.arduino.write(command.encode('utf-8'))
+                        time.sleep(0.1)
+                        response = self.arduino.readline().decode().strip()
+                        if response == "200":
+                            self._logger.info("Fan Speed changed: " + command)
+                            self.RequestCommandProcess = False
+                            self.RequestCommandProcess = False
+                            return jsonify(success=True)
+                            
+            except:
+                self.RequestCommandProcess = False
+                self._logger.error("Enclosure Thermostat Encountered an Issue: 2")
+                self.RequestCommandProcess = False
+                return jsonify(success=False)
+        
+    def get_serialconnectcheck(self):
+        try:
+            self.arduino.inWaiting()
+        except:
+            self._logger.error("Enclosure Thermostat Not Connected.. Retrying Connection")
+            self.connect_serial_thermo()
+       
+    def start_serialconnectioncheck_timer(self, interval):
+        self._checkTempTimer = RepeatedTimer(interval, self.get_serialconnectcheck, run_first=True)
+        self._checkTempTimer.start() 
+ 
+    def on_after_startup(self):
+        self._logger.info("Enclosure Thermostat Started.")
+        self.comport = self._settings.get(["comport"])
+        self.baudrate = self._settings.get(["baudrate"])
+        self.showenclosuretemp = self._settings.get(["showenclosuretemp"])
+        self.showmode = self._settings.get(["showmode"])
+        self.showstatus = self._settings.get(["showstatus"])
+        self.showtargettemp = self._settings.get(["showtargettemp"])
+        self.start_serialconnectioncheck_timer(10)
+        
+    def stop_tempcheck_timer(self):
+        self._checkTempTimer.cancel
+        
+    def get_enclosure_temp(self):
+        if (self.RequestCommandProcess == False):
+            self.RequestCommandProcess = True
+            #Get Temp
+            try:
+                if self.serialconnected:
+                    command = "<SInternalTemp>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    self.temp = response
+                    self.arduino.flush()
+                    self._logger.info("Enclosure Temp: " + self.temp)
+                    self._plugin_manager.send_plugin_message(self._identifier,
+                                                             dict(enclosureTemp=str(self.temp) + '\u00b0F'))
+            except:
+                self._logger.error("Enclosure Thermostat Not Connected.. Retrying Connection")
+                self.serialconnected = False
+                self.connect_serial_thermo()
+            #Get Mode
+            try:
+                if self.serialconnected:
+                    command = "<SMode>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    self.mode = response
+                    self.arduino.flush()
+                    self._logger.info("Enclosure Mode: " + self.mode)
+                    self._plugin_manager.send_plugin_message(self._identifier,
+                                                             dict(enclosureMode=str(self.mode)))
+            except:
+                self._logger.error("Enclosure Thermostat Not Connected.. Retrying Connection")
+                self.serialconnected = False
+                self.connect_serial_thermo()
+            #Get Status
+            try:
+                if self.serialconnected:
+                    command = "<SStatus>"
+                    self.arduino.write(command.encode('utf-8'))
+                    time.sleep(0.1)
+                    response = self.arduino.readline().decode().strip()
+                    self.status = response
+                    self.arduino.flush()   
+                    self._logger.info("Enclosure Status: " +self.status)
+                    self._plugin_manager.send_plugin_message(self._identifier,
+                                                             dict(enclosureStatus=str(self.status)))
+            except:
+                self._logger.error("Enclosure Thermostat Not Connected.. Retrying Connection")
+                self.serialconnected = False
+                self.connect_serial_thermo()
+            #Get TargetTemp
+            try:
+                if self.serialconnected:
+                    if (self.mode == "FILA"):
+                        command = "<SFilamentTemp>"
+                        self.arduino.write(command.encode('utf-8'))
+                        time.sleep(0.1)
+                        response = self.arduino.readline().decode().strip()
+                        self.TargetTemp = response
+                        self.arduino.flush()   
+                        self._logger.info("Target Temp: " + self.TargetTemp)
+                        self._plugin_manager.send_plugin_message(self._identifier,
+                                                                 dict(enclosuretargettemp=str(self.TargetTemp)))
+                    elif (self.mode == "TEMP"):
+                        command = "<SManualTargetTemp>"
+                        self.arduino.write(command.encode('utf-8'))
+                        time.sleep(0.1)
+                        response = self.arduino.readline().decode().strip()
+                        self.TargetTemp = response
+                        self.arduino.flush()   
+                        self._logger.info("Target Temp: " + self.TargetTemp)
+                        self._plugin_manager.send_plugin_message(self._identifier,
+                                                                 dict(enclosuretargettemp=str(self.TargetTemp)))
+                    else:
+                        self._logger.info("Target Temp: None")
+                        self._plugin_manager.send_plugin_message(self._identifier,
+                                                                 dict(enclosuretargettemp="None"))
+
+            except:
+                self._logger.error("Enclosure Thermostat Not Connected.. Retrying Connection")
+                self.serialconnected = False
+                self.connect_serial_thermo()
+            self.RequestCommandProcess = False
+                                                 
+    def get_enclosure_mode(self):
+        self._logger.info("Getting Enclosure Mode..")
+        self._plugin_manager.send_plugin_message(self._identifier,
+                                                 dict(enclosureMode='Enclosure Mode: ' + str(self.mode)))
+    def get_enclosure_target(self):
+        self._logger.info("Getting Enclosure Target..")
+        self._plugin_manager.send_plugin_message(self._identifier,
+                                                dict(enclosureMode='Enclosure Target: ' + str(self.target)))
+ 
+    def get_template_configs(self):
+        try:
+            return [
+                dict(type="navbar", template="enclosurethermostat_navbar.jinja2"),
+                dict(type="settings", template="enclosurethermostat_settings.jinja2"),
+                dict(type="tab", template="enclosurethermostat_tab.jinja2")
+            ]
+        except Exception:
+            return []
+            
+    def get_settings_defaults(self):
+        return dict(comport="/dev/Temp_Controller", baudrate="9600", showenclosuretemp=True, showmode=True, showstatus=True, showtargettemp=True)
+        
+ 
+    def get_assets(self):
+        return dict(
+            js=["js/enclosurethermostat.js", "js/plotly-2.12.1.min.js"],
+            css=["css/enclosurethermostat.css"]
+        )
+
+__plugin_name__ = "Enclosurethermostat"
+__plugin_pythoncompat__ = ">=3.7,<4"
+__plugin_implementation__ = EnclosurethermostatPlugin()
---
+++ octoprint_EnclosureThermostat/static/js/enclosurethermostat.js
@@ -0,0 +1,183 @@
+$(function() {
+    function EnclosureThermostatViewModel(parameters) {
+        var self = this;
+		self.pluginName = "EnclosureThermostat";
+		self.global_settings = parameters[0];
+		self.EnclTemp = ko.observable("");
+		self.CurMode = ko.observable("");
+		self.EnclStatus = ko.observable("");
+		self.TargetTempVal = ko.observable("");
+		self.TargetTempVis = ko.observable(false);
+		self.GlobalTargTemp = ""
+		self.newArrayY = ko.observableArray();
+		self.newArrayX = ko.observableArray();
+		self.ChartData = ko.observableArray();
+		
+		self.onBeforeBinding = function () {
+            self.settings = self.global_settings.settings.plugins.EnclosureThermostat;
+        };
+				
+		self.thermostatoff = function() {
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				url: self.buildPluginUrl("/thermostatoff")
+	//        		success: function (data) {
+	//          			self.getUpdateUI();
+	//        		}
+			});
+		};
+		self.thermostatfilmode = function() {
+
+			mode = $("#FilamentType").val();
+			var request = {"mode": mode};
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				data: request,
+				url: self.buildPluginUrl("/thermostatfilament")
+	//        		success: function (data) {
+	//          			self.getUpdateUI();
+	//        		}
+			});
+		};
+
+		self.thermostatmantemp = function() {
+			tempval = $("#mantempval").val();
+			try {
+				tempval = parseInt(tempval);
+				let comparenumber = Number.isFinite(tempval);
+				if (comparenumber){
+					if (tempval >60 && tempval < 121){					
+						var request = {"tempval": tempval};
+						$.ajax({
+							type: "GET",
+							dataType: "json",
+							data: request,
+							url: self.buildPluginUrl("/thermostatmantemp")
+				//        		success: function (data) {
+				//          			self.getUpdateUI();
+				//        		}
+						});
+				}
+			}
+			} catch (error) {
+
+			}				
+		};
+		
+		self.thermostatmanpwm = function() {
+			pwmtempval = $("#manpwmval").val();
+			try {
+				pwmtempval = parseInt(pwmtempval);
+				let comparenumber = Number.isFinite(pwmtempval);
+				if (comparenumber){
+					if (pwmtempval >= 0 && pwmtempval <= 100){					
+						var request = {"tempval": pwmtempval};
+						$.ajax({
+							type: "GET",
+							dataType: "json",
+							data: request,
+							url: self.buildPluginUrl("/thermostatmanpwm")
+				//        		success: function (data) {
+				//          			self.getUpdateUI();
+				//        		}
+						});
+				}
+			}
+			} catch (error) {
+
+			}				
+		};		
+	
+		self.onDataUpdaterPluginMessage = function(plugin, data) {
+			if (plugin != "EnclosureThermostat") {
+				return;
+			}
+			if (data.enclosureTemp){
+				self.EnclTemp(data.enclosureTemp)
+			}
+			if (data.enclosuretargettemp){
+				if (self.GlobalTargTemp == "FILA" || self.GlobalTargTemp == "TEMP"){
+					self.TargetTempVis(true);
+					self.TargetTempVal(String(data.enclosuretargettemp));
+				} else {
+					self.TargetTempVis(false);
+				}	
+			}
+			if (data.enclosureMode){
+				self.GlobalTargTemp = String(data.enclosureMode);
+				self.CurMode(data.enclosureMode)
+			}
+			if (data.enclosureStatus){
+				self.EnclStatus(data.enclosureStatus)
+				self.EnclosureStatus = String(data.enclosureStatus)
+				if (self.EnclosureStatus == "Cooling") {
+					$("#EnclosureStatusValue").attr('class', 'cooling');
+					console.log("cooling");
+				} else if (self.EnclosureStatus == "Heating") {
+					$("#EnclosureStatusValue").attr('class', 'heating');
+					console.log("heating");
+				} else  {
+					$("#EnclosureStatusValue").attr('class', 'regular');
+					console.log("regular");
+				}
+			}
+        }
+		self.buildPluginUrl = function (path) {
+		  return window.PLUGIN_BASEURL + self.pluginName + path;
+		};
+	//Graph
+    var arrayLength = 60
+	//var newArrayY = []
+	//var newArrayX = []
+	
+	
+	for(var i = 0; i < arrayLength; i++) {
+		var x = new Date();
+		self.newArrayX().push(x);
+		var y = self.EnclTemp(); //Math.round(parseInt(self.EnclTemp()));
+		self.newArrayY().push(y);
+	}
+
+	Plotly.newPlot('graph', [{
+	  x: [self.newArrayX()], 
+	  y: [self.newArrayY()],
+	  mode: 'lines',
+	  line: {color: '#80CAF6'}
+	}]);  
+
+	var cnt = 0;
+
+	var interval = setInterval(function() {
+	  var tempint = parseInt(self.EnclTemp());
+	  let comparetemp = Number.isFinite(tempint);
+		if (comparetemp){
+		  var x = new Date();
+		  self.newArrayX().push(x)
+		  self.newArrayX().splice(0, 1)	  
+		  var y = self.EnclTemp();
+		  self.newArrayY().push(y)
+		  self.newArrayY().splice(0, 1)
+		  
+		  var data_update = {
+		  x: [self.newArrayX()],
+		  y: [self.newArrayY()]
+		  };
+		  
+		  Plotly.update('graph', data_update)
+		} 
+	  if(cnt === 100) clearInterval(interval);
+	}, 5000);
+	
+	
+	
+    }
+
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: EnclosureThermostatViewModel,
+		dependencies: ["settingsViewModel"],
+        elements: ["#navbar_plugin_EnclosureThermostat", "#settings_plugin_EnclosureThermostat", "#tab_plugin_EnclosureThermostat"]
+    });
+});
